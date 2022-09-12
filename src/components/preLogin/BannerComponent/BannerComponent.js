b59516f1e5d8e7dc85de32db976c887481e2ebf0
chandrashekhar maity <57037635+EntityZeus@users.noreply.github.com>
2022-09-13 00:09:53 +0530

    login page

--- src/components/preLogin/BannerComponent/BannerComponent.js
+++ src/components/preLogin/BannerComponent/BannerComponent.js
@@ -1,14 +1,47 @@
 import styleSheet from './BannerComponent.module.css';
+import { useState } from 'react';
 
 export default function BannerComponent(){
+    const [userEmail,setUserEmail]=useState('');
+    const [password,setPassword]=useState('');
+
+    const onEmailEntered=(event)=>{
+        setUserEmail(event.target.value);
+    }
+    const onPasswordEntered=(event)=>{
+        setPassword(event.target.value);
+    }
+
+    let userDetails={};
+
+    const submitForm=()=>{
+        userDetails['email']=userEmail;
+        userDetails['password']=password;
+        console.log(userDetails);
+    }
+
     return(
         <div className={`row ${styleSheet.backgroundImage}`}>
             <div className='col-md-6 offset-md-3' style={{marginTop:'14%'}}>
-            <div className="card text-center" style={{width : '100%'}}>
+            <div className="card" style={{width : '100%', background:'transparent', color:'white', border:'none'}}>
                 <div className="card-body">
-                    <h5 className="card-title">Special title treatment</h5>
-                    <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                    <button className="btn btn-primary">Go somewhere</button>
+                    <form>
+                        <div className="form-group">
+                            <label>Email address</label>
+                            <input type="email" className="form-control"
+                            placeholder='Enter Your Email ID' onKeyUp={onEmailEntered} />
+                        </div>
+                        <div className="form-group">
+                            <label>Password</label>
+                            <input type="password" className="form-control" 
+                            placeholder='Enter Your Password' onKeyUp={onPasswordEntered}/>
+                        </div>
+                        <div className="form-group form-check">
+                            <input type="checkbox" className="form-check-input" id="exampleCheck1" />
+                            <label className="form-check-label" >Check me out</label>
+                        </div>
+                        <button type="button" className="btn btn-outline-light" onClick={submitForm}>Submit</button>
+                    </form>
                 </div>
             </div>
             </div>
--- src/components/preLogin/PreLoginComponent.js
+++ src/components/preLogin/PreLoginComponent.js
@@ -4,13 +4,37 @@ import UserLogin from './userLogin/UserLoginComponent';
 
 export default function PreLoginComponent(){
     return (
-        <div className='row' style={{width:'100%'}}>
-            <div className='col-md-12 col-sm-12'>
-                <BannerComponent />
+        <>
+            <nav className={`navbar navbar-expand-lg navbar-dark ${styleSheet['bg-dark']}`} style={{backgroundColor: '#24292d !important'}}>
+                <a className="navbar-brand" href="#">Navbar</a>
+                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarNav">
+                    <ul className="navbar-nav">
+                    <li className="nav-item active">
+                        <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="#">Features</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="#">Pricing</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link disabled">Disabled</a>
+                    </li>
+                    </ul>
+                </div>
+            </nav>
+            <div className='row' style={{width:'100%'}}>
+                <div className='col-md-12 col-sm-12'>
+                    <BannerComponent />
+                </div>
+                <div className='col-md-12 col-sm-12'>
+                    <UserLogin />    
+                </div>
             </div>
-            <div className='col-md-12 col-sm-12'>
-                <UserLogin />    
-            </div>
-        </div>
+        </>
     )
 }
